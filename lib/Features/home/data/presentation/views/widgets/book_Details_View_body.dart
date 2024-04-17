91aaff0ab6a446adc48bbae09299aad6069b534b
Ahmedshams1 <ahmedsama111997@gmail.com>
2024-04-17 19:52:25 +0200

    create search view

--- lib/Features/home/data/presentation/views/widgets/book_Details_View_body.dart
+++ lib/Features/home/data/presentation/views/widgets/book_Details_View_body.dart
@@ -1,10 +1,6 @@
 import 'package:bookly/Features/home/data/presentation/views/widgets/Book_details_section.dart';
-import 'package:bookly/Features/home/data/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/Features/home/data/presentation/views/widgets/custom_Book_Details_AppBar.dart';
-import 'package:bookly/Features/home/data/presentation/views/widgets/custom_book_image.dart';
-import 'package:bookly/Features/home/data/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:bookly/Features/home/data/presentation/views/widgets/similar_books_section.dart';
-import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
--- lib/core/utils/App_rauter.dart
+++ lib/core/utils/App_rauter.dart
@@ -1,17 +1,23 @@
 import 'package:bookly/Features/home/data/presentation/views/book_details_View.dart';
 import 'package:bookly/Features/home/data/presentation/views/home_view.dart';
+import 'package:bookly/Features/search/presentation/views/search_view.dart';
 import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const KHomeView = '/HomeView';
   static const KBookDetailsView = '/bookDetailsView';
+  static const KSearchView = '/searchView';
 
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => SplashView(),
+      ),
+      GoRoute(
+        path: KSearchView,
+        builder: (context, state) => SearchView(),
       ),
       GoRoute(
         path: KHomeView,
